2a42d84fc19f2f8fc93c6a141b7c39f43ca0bcb4
Al Sweigart <asweigart@gmail.com>
2019-10-09 11:36:36 -0700

    Ran Black formatter over setup.py.

--- {{cookiecutter.module_name}}/setup.py
+++ {{cookiecutter.module_name}}/setup.py
@@ -7,35 +7,36 @@ scriptFolder = os.path.dirname(os.path.realpath(__file__))
 os.chdir(scriptFolder)
 
 # Find version info from module (without importing the module):
-with open('src/{{ cookiecutter.module_name }}/__init__.py', 'r') as fileObj:
-    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-                        fileObj.read(), re.MULTILINE).group(1)
+with open("src/{{ cookiecutter.module_name }}/__init__.py", "r") as fileObj:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fileObj.read(), re.MULTILINE
+    ).group(1)
 
 # Use the README.md content for the long description:
-with io.open('README.md', encoding='utf-8') as fileObj:
+with io.open("README.md", encoding="utf-8") as fileObj:
     long_description = fileObj.read()
 
 setup(
-    name='{{ cookiecutter.project_name }}',
+    name="{{ cookiecutter.project_name }}",
     version=version,
-    url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.module_name }}',
-    author='{{ cookiecutter.author_name }}',
-    author_email='{{ cookiecutter.author_email }}',
-    description=('''{{ cookiecutter.project_short_description }}'''),
+    url="https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.module_name }}",
+    author="{{ cookiecutter.author_name }}",
+    author_email="{{ cookiecutter.author_email }}",
+    description=("""{{ cookiecutter.project_short_description }}"""),
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    license='MIT',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    test_suite='tests',
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    test_suite="tests",
     install_requires=[],
-    keywords='',
+    keywords="",
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
 )
