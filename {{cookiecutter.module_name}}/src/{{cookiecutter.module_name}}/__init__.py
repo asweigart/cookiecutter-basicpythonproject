bb6fe7ec390bc962015b3eef62de0b73e50f5e97
Al Sweigart <asweigart@gmail.com>
2020-04-27 00:33:44 -0700

    Using a module docstring in __init__.py instead of single-line comments.

--- {{cookiecutter.module_name}}/src/{{cookiecutter.module_name}}/__init__.py
+++ {{cookiecutter.module_name}}/src/{{cookiecutter.module_name}}/__init__.py
@@ -1,4 +1,6 @@
-# {{ cookiecutter.project_name }}
-# By {{ cookiecutter.author_name }} {{ cookiecutter.author_email }}
+"""{{ cookiecutter.project_name }}
+By {{ cookiecutter.author_name }} {{ cookiecutter.author_email }}
 
-__version__ = '{{cookiecutter.project_version}}'
+{{ cookiecutter.author_email }}"""
+
+__version__ = '{{cookiecutter.project_short_description}}'
