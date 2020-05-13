d18eca0ce2adbfde1cbf2436a87cfec77126a120
Al Sweigart <asweigart@gmail.com>
2020-05-13 01:56:10 -0700

    Correcting typo.

--- {{cookiecutter.module_name}}/src/{{cookiecutter.module_name}}/__init__.py
+++ {{cookiecutter.module_name}}/src/{{cookiecutter.module_name}}/__init__.py
@@ -1,6 +1,6 @@
 """{{ cookiecutter.project_name }}
 By {{ cookiecutter.author_name }} {{ cookiecutter.author_email }}
 
-{{ cookiecutter.author_email }}"""
+{{ cookiecutter.project_short_description }}"""
 
-__version__ = '{{cookiecutter.project_short_description}}'
+__version__ = '{{cookiecutter.version}}'
